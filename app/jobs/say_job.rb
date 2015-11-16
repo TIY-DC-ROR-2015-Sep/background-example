b2866418d2d3841868d9944bf73bb1537ad7870e
James Dabbs <jamesdabbs@gmail.com>
2015-11-16 10:39:48 -0500

    Add failure and look at retries

--- app/jobs/say_job.rb
+++ app/jobs/say_job.rb
@@ -1,8 +1,12 @@
 class SayJob < ActiveJob::Base
   queue_as :default
 
-  def perform phrase
-    sleep 1
+  def perform phrase, sleep_time
+    puts "Sleeping for #{sleep_time}"
+    sleep sleep_time
+    if rand(1 .. 10) < 3
+      1 / 0
+    end
     system "say", phrase
   end
 end
